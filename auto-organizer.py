828ab6f63b1dd42a97acefd8cb3b32138bee9108
Marco Dominguez <47532404+MarcoDomi@users.noreply.github.com>
2025-11-29 01:26:38 -0600

    comment out get_init_dir()
    for now this project will focus solely on the
    downloads directory found within the home directory.

--- auto-organizer.py
+++ auto-organizer.py
@@ -4,14 +4,37 @@ import shutil
 import sys
 from pathlib import Path
 
+HOME_DIR = Path.home()
 
-if len(sys.argv) > 1 and sys.argv[1] == 'cwd': #set INIT_DIR to the current working directory
-    INIT_DIR = Path.cwd()
-else:
-    INIT_DIR = Path.home() #path where the download files will be moved to.
+'''def get_init_dir(): #NOTE i forgot why i added this :(
+    if len(sys.argv) > 1 and sys.argv[1] == 'cwd': #set INIT_DIR to the current working directory
+        INIT_DIR = Path.cwd()
+    else:
+        INIT_DIR = HOME_DIR #path where the download files will be moved to.
+
+    return INIT_DIR'''
+
+
+def move_file(file): 
+    #values as sets allow for efficient membership testing
+    valid_extensions = {
+        'documents': set('.txt', '.pdf', '.docx'),
+        'archives': set('.zip', '.tar.gz'),
+        'videos': set('.mp4', '.mkv', '.mov'),
+        'images': set('.jpg', '.jpeg', '.png', '.gif')
+    }
+
+
+def scan_downloads():
+    '''scan download directory for files'''
+
+    download_dir = HOME_DIR.joinpath('downloads')
+    for f in download_dir.iterdir():
+        move_file(f)
 
 
 def main():
-    print(INIT_DIR)
+    #INIT_DIR = get_init_dir()
+    scan_downloads()
 
 main()
