e093d43633ca700488abc3ebb6a25b2ef023ac48
Marco Dominguez <47532404+MarcoDomi@users.noreply.github.com>
2025-12-02 01:35:10 -0600

    create create_path() function

--- auto-organizer.py
+++ auto-organizer.py
@@ -14,9 +14,15 @@ DOWNLOAD_DIR = Path.home() / "downloads"
 
     return INIT_DIR'''
 
+def create_path(path):
+    try:
+        path.mkdir()
+    except FileExistsError:
+        print(f"Directory {path} already exists")
+
 
 def move_file(file): 
-    #values as sets allow for efficient membership testing
+    # values as sets allow for efficient membership testing
     valid_extensions = {
         'documents': {'.txt', '.pdf', '.docx'},
         'archives': {'.zip', '.tar.gz'},
@@ -24,9 +30,11 @@ def move_file(file):
         'images': {'.jpg', '.jpeg', '.png', '.gif'}
     }
 
-    for file_type, file_ext in valid_extensions.items():
-        if file.suffix in file_ext:
-            print(file_type)
+    for file_type, f_extension in valid_extensions.items():
+        type_path = DOWNLOAD_DIR.joinpath(file_type)
+        if file.suffix in f_extension:
+            create_path(type_path)
+            
 
 
 def scan_downloads():
