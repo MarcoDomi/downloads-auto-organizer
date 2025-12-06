20469509a34fe26545457688a8cdecc018c28e99
Marco Dominguez <47532404+MarcoDomi@users.noreply.github.com>
2025-12-06 14:46:12 -0600

    move valid_extensions to global scope
    making valid_extensions a global variable makes it
    possible for future functions and data structures
    to access the dictionary of extensions

--- auto-organizer.py
+++ auto-organizer.py
@@ -6,6 +6,15 @@ from pathlib import Path
 
 DOWNLOAD_DIR = Path.home() / "downloads"
 
+ # values as sets allow for efficient membership testing
+valid_extensions = {
+    'documents': {'.txt', '.pdf', '.docx'},
+    'archives': {'.zip', '.tar.gz'},
+    'videos': {'.mp4', '.mkv', '.mov'},
+    'images': {'.jpg', '.jpeg', '.png', '.gif'},
+    'audio' : {'.mp3'}
+}
+
 '''def get_init_dir(): #NOTE i forgot why i added this :(
     if len(sys.argv) > 1 and sys.argv[1] == 'cwd': #set INIT_DIR to the current working directory
         INIT_DIR = Path.cwd()
@@ -13,6 +22,7 @@ DOWNLOAD_DIR = Path.home() / "downloads"
         INIT_DIR = HOME_DIR #path where the download files will be moved to.
 
     return INIT_DIR'''
+
 
 def create_path(dir_path):
     try:
@@ -22,14 +32,6 @@ def create_path(dir_path):
 
 
 def move_file(file): 
-    # values as sets allow for efficient membership testing
-    valid_extensions = {
-        'documents': {'.txt', '.pdf', '.docx'},
-        'archives': {'.zip', '.tar.gz'},
-        'videos': {'.mp4', '.mkv', '.mov'},
-        'images': {'.jpg', '.jpeg', '.png', '.gif'},
-        'audio' : {'.mp3'}
-    }
 
     for file_type, f_extension in valid_extensions.items():
         type_path = DOWNLOAD_DIR.joinpath(file_type)
