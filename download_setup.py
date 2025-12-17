85a887670c43036b47c7bebbe09266b99d9a5da5
Marco Dominguez <47532404+MarcoDomi@users.noreply.github.com>
2025-12-17 16:49:59 -0600

    enable clearing log file using download_setup.py

--- download_setup.py
+++ download_setup.py
@@ -1,21 +1,25 @@
 #! /usr/bin/python3
-#populates downloads directory with files
+# populates downloads directory with files
 from pathlib import Path
 
-download_dir = Path.home() / 'downloads'
+download_dir = Path.home() / 'downloads' 
 files = ('hi.txt', 'friend.txt', 'funny.mov', 'metallica.mp3','show_ep1.mov','dog.jpg','study.pdf', 'history.tar.gz')
 
-def clear_dir(parent_dir):
+
+def clear_dir(parent_dir):  # NOTE be very careful when deleting multiple directories
+    '''delete all files w/i directory'''
     for file in parent_dir.iterdir():
         file.unlink()
         print(f"{file.name} deleted")
 
 
-for f in Path.iterdir(download_dir):
+for f in Path.iterdir(download_dir): #remove all directories w/i the submitted directory
     if f.is_dir():
         clear_dir(f)
         f.rmdir()
 
-for f in files:
+for f in files:  #repopulate downloads with files
     file_path = download_dir / f
     file_path.touch()
+
+open('file_move_log.txt', 'w').close() #clear content in file_move_log.txt
