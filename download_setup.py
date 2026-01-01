d5c1b09a4c55334a9166c816d4d3fef154425083
Marco Dominguez <47532404+MarcoDomi@users.noreply.github.com>
2026-01-01 15:00:39 -0600

    remove directory/file deleting capabilites
    removes the risk associated with automatically
    deleting files and directories

--- download_setup.py
+++ download_setup.py
@@ -6,17 +6,7 @@ download_dir = Path.home() / 'downloads'
 files = ('hi.txt', 'friend.txt', 'funny.mov', 'metallica.mp3','show_ep1.mov','dog.jpg','study.pdf', 'history.tar.gz')
 
 
-def clear_dir(parent_dir):  # NOTE be very careful when deleting multiple directories
-    '''delete all files w/i directory'''
-    for file in parent_dir.iterdir():
-        file.unlink()
-        print(f"{file.name} deleted")
 
-
-for f in Path.iterdir(download_dir): #remove all directories w/i the submitted directory
-    if f.is_dir():
-        clear_dir(f)
-        f.rmdir()
 
 for f in files:  #repopulate downloads with files
     file_path = download_dir / f
